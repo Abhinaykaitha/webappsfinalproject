11a4c85da8772b4cdcd1d18836c7b524e00a888c
Abhinay <abhinayreddy1278@gmail.com>
2018-11-13 18:52:31 -0600

    Abhinay Commit

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,6 +1,7 @@
 const Datastore = require('nedb')
 const LOG = require('../utils/logger.js')
 const products = require('../data/product.json')
+const customers = require('../data/customer.json')
 
 module.exports = (app) => {
   LOG.info('START seeder.')
@@ -27,5 +28,11 @@ module.exports = (app) => {
   //app.locals.orders = db.orders.find(orders)
   //LOG.debug(`${app.locals.orders.query.length} orders seeded`)
   
+  LOG.info('END Seeder. Sample data read and verified.')
+  db.customers= new Datastore()
+  db.customers.loadDatabase()
+  db.customers.insert(customers)
+  app.locals.customers = db.customers.find(customers)
+  LOG.debug(`${app.locals.customers.query.length} customers seeded`)
   LOG.info('END Seeder. Sample data read and verified.')
 }
